ec8982b77fbe199836fca672bba414b738d1f976
hai <thinkwan@qq.com>
2021-03-24 10:49:20 +0800

    Site updated: 2021-03-24 10:49:18

--- assets/apt-install.sh
+++ assets/apt-install.sh
@@ -12,4 +12,5 @@ sed -i '0,/plugins=(git)/s/plugins=(git)/plugins=(git z tmux)/' .zshrc&&
 #:解决nice(5) failed: operation not permitted
 sudo sed -i '$a   \unsetopt BG_NICE' .zshrc &&
 sudo source .zshrc &&
+chsh -s $(which zsh)&&
 echo "Updated oh-my-zsh plugins";
